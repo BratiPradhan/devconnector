96d2021b98ac7d7bbeec9b95f686e7bb7c082493
BratiPradhan <pradhanbrati@gmail.com>
2020-10-08 10:46:58 +0530

    Delete Education, Experiences and Account

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -7,7 +7,7 @@ const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User');
 const Profile = require('../../models/Profile');
-const { response } = require('express');
+const Post = require('../../models/Post');
 
 // @route   GET api/profile/me
 // @desc    Get current user's profile
@@ -148,7 +148,8 @@ router.get('/user/:user_id', async (req, res) => {
 // @access  Private
 router.delete('/', auth, async (req, res) => {
   try {
-    // @todo- remove users posts
+    // Remove user posts
+    await Post.deleteMany({ user: req.user.id });
 
     // Remove profile
     await Profile.findOneAndRemove({ user: req.user.id });
