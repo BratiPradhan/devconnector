bc7cad9d20242b8117e27ca890efacb9eaa1285c
BratiPradhan <pradhanbrati@gmail.com>
2020-11-21 17:12:52 +0530

    Comment Display and Delete

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -1,11 +1,13 @@
 import React, { Fragment, useEffect } from 'react'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import Spinner from '../layout/Spinner'
 import PostItem from '../posts/PostItem'
 import CommentForm from './CommentForm'
+import CommentItem from './CommentItem'
 import { getPost } from '../../actions/post'
-import { Link } from 'react-router-dom'
+
 
 
 const Post = ({ getPost, post: { post, loading }, match }) => {
@@ -19,6 +21,11 @@ const Post = ({ getPost, post: { post, loading }, match }) => {
         </Link>
         <PostItem post={post} showActions={false}/>
         <CommentForm postId={post._id}/>
+        <div className="comment">
+            {post.comments.map(comment => (
+                <CommentItem key={comment._id} comment={comment} postId={post._id}/>
+            ))}
+        </div>
     </Fragment>
 }
 
