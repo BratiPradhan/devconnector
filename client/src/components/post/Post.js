0af0c67824a18053a8b17e917c4ddd3ceb4ba7f6
BratiPradhan <pradhanbrati@gmail.com>
2020-11-21 15:32:38 +0530

    Adding Comments

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -3,8 +3,10 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import Spinner from '../layout/Spinner'
 import PostItem from '../posts/PostItem'
+import CommentForm from './CommentForm'
 import { getPost } from '../../actions/post'
 import { Link } from 'react-router-dom'
+
 
 const Post = ({ getPost, post: { post, loading }, match }) => {
     useEffect(()  => {
@@ -16,6 +18,7 @@ const Post = ({ getPost, post: { post, loading }, match }) => {
             Back To Posts
         </Link>
         <PostItem post={post} showActions={false}/>
+        <CommentForm postId={post._id}/>
     </Fragment>
 }
 
